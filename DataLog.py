d6d86d839bdf32f4e8063b25918c653236ca8d47
xpbim3 <Xpbim3@gmail.com>
2022-09-19 02:17:36 +0500

    Massive cleanup

--- DataLog.py
+++ DataLog.py
@@ -1,65 +1,47 @@
 import numpy as np
 np.set_printoptions(floatmode = 'fixed',precision = 2, linewidth = 150, suppress = True)
-#from xml_test import mstable
 import glob
 import pandas as pd
-#import pywebio
-#from pywebio.output import *
 import scipy
-from scipy import interpolate
-from scipy.ndimage.filters import gaussian_filter
 
 from TuneParser import *
-#import matplotlib.pyplot as plt
-#fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
 
 #USER settings section
 
 SHIFT_AFR = 1
 HITS_NEEDED = 10
-T_FULLY_WARMED = 60
-STOICH = 14.7
 QUANTILE = 0.75
-BLURR_FACTOR = 0.5
+MARGIN=1.0
+
+
+
 CONFIG_ATMO_6 = {'KPA_MIN':24, 'KPA_MAX':100, 'RPM_MIN':400,'RPM_MAX':6000, 'AFR_MIN':14.5, 'AFR_MAX':12.5}
 CONFIG_ATMO_M20 = {'KPA_MIN':28, 'KPA_MAX':100, 'RPM_MIN':400,'RPM_MAX':6000, 'AFR_MIN':14.5, 'AFR_MAX':12.5}
 CONFIG_TURBO_M20 = {'KPA_MIN':28, 'KPA_MAX':180, 'RPM_MIN':400,'RPM_MAX':6000, 'AFR_MIN':14.0, 'AFR_MAX':12.0}
 CONFIG_ATMO_VWKR = {'KPA_MIN':20, 'KPA_MAX':100, 'RPM_MIN':600,'RPM_MAX':6000, 'AFR_MIN':14.5, 'AFR_MAX':12.5}
-
-CONFIG_TURBO_6 = {'KPA_MIN':24, 'KPA_MAX':230, 'RPM_MIN':400,'RPM_MAX':6000, 'AFR_MIN':14.5, 'AFR_MAX':12.0}
 CONFIG_TURBO_SR20 = {'KPA_MIN':24, 'KPA_MAX':200, 'RPM_MIN':600,'RPM_MAX':7000, 'AFR_MIN':14.5, 'AFR_MAX':11.0}
 
 
 CURRENT_CONFIG = CONFIG_TURBO_M20
 
 
-#KPA_BINS = np.linspace(25,100,16)
-#RPM_BINS = np.linspace(400,7000,16)
-
-#KPA_BINS = ((np.round(np.linspace(CURRENT_CONFIG['KPA_MIN'],CURRENT_CONFIG['KPA_MAX'], 16))//2)*2).astype(int)
-#RPM_BINS = ((np.round(np.linspace(CURRENT_CONFIG['RPM_MIN'],CURRENT_CONFIG['RPM_MAX'], 16))//100)*100).astype(int)
-
-
-
-
-
 VE_TABLE_DICT = {'table': 'veTable', 'xaxis': 'rpmBins', 'yaxis': 'fuelLoadBins'}
 AFR_TABLE_DICT = {'table': 'afrTable', 'xaxis': 'rpmBinsAFR', 'yaxis': 'loadBinsAFR'}
 IGN_TABLE_DICT = {'table': 'advTable1', 'xaxis': 'rpmBins2', 'yaxis': 'mapBins1'}
 F_NAME = 'CurrentTune.msq'
-RPM_BINS_VE, KPA_BINS_VE, table, VE_TABLE_FUNC = getTable(F_NAME,VE_TABLE_DICT)
-RPM_BINS_AFR, KPA_BINS_AFR, table, AFR_TABLE_FUNC = getTable(F_NAME,AFR_TABLE_DICT)
-#RPM_BINS_IGN, KPA_BINS_IGN, table, IGN_TABLE_FUNC = getTable(F_NAME,IGN_TABLE_DICT)
+RPM_BINS_VE, KPA_BINS_VE, _, VE_TABLE_FUNC = getTable(F_NAME,VE_TABLE_DICT)
+RPM_BINS_AFR, KPA_BINS_AFR, _, AFR_TABLE_FUNC = getTable(F_NAME,AFR_TABLE_DICT)
+RPM_BINS_IGN, KPA_BINS_IGN, _, IGN_TABLE_FUNC = getTable(F_NAME,IGN_TABLE_DICT)
 
 
 KPA_BINS = ((np.round(np.linspace(CURRENT_CONFIG['KPA_MIN'],CURRENT_CONFIG['KPA_MAX'], 16))//2)*2).astype(int)
 RPM_BINS = ((np.round(np.linspace(CURRENT_CONFIG['RPM_MIN'],CURRENT_CONFIG['RPM_MAX'], 16))//100)*100).astype(int)
-#KPA_BINS = KPA_BINS_VE
-#№RPM_BINS = RPM_BINS_VE
+KPA_MARGIN = ((CURRENT_CONFIG['KPA_MAX'] - CURRENT_CONFIG['KPA_MIN'])//15)*MARGIN
+RPM_MARGIN = ((CURRENT_CONFIG['RPM_MAX'] - CURRENT_CONFIG['RPM_MIN'])//15)*MARGIN
 
-
-
+print(f"KPA margin: {KPA_MARGIN}")
+print(f"RPM margin: {RPM_MARGIN}")
 print("bins:")
 print(KPA_BINS)
 print(RPM_BINS)
@@ -67,18 +49,12 @@ print(RPM_BINS)
 
 VE_TABLE = VE_TABLE_FUNC(RPM_BINS, KPA_BINS)
 #IGN_TABLE = IGN_TABLE_FUNC(RPM_BINS, KPA_BINS)
-
 AFR_TABLE = np.array([np.linspace(CURRENT_CONFIG['AFR_MIN'], CURRENT_CONFIG['AFR_MAX'], 16)]*16).transpose()
 #AFR_TABLE = AFR_TABLE_FUNC(RPM_BINS, KPA_BINS)
 AFR_TABLE_FUNC = scipy.interpolate.interp2d(RPM_BINS, KPA_BINS, AFR_TABLE)
 
 
 
-def pywebioTableRepresentation(table:np.ndarray, xaxis:np.ndarray, yaxis:np.ndarray):
-	ret = np.hstack([yaxis.reshape((16,1)), table])
-	xaxis_0 = np.hstack([0, xaxis]).reshape((1,17))
-	ret = np.vstack([xaxis_0, ret])
-	return np.flipud(ret)
 
 
 
@@ -106,20 +82,6 @@ def getBinFromValue(value:float, bins:np.ndarray):
 	return np.argmin(np.abs(value-bins))
 
 
-def getValueFromBin(val:int, bins:np.ndarray):
-	midvalues = []
-	for i in range(bins.size-1):
-		midvalues.append((bins[i]+bins[i+1])/2)
-	CONST=0
-	midvalues = [0.0] + midvalues + [bins[-1]*2.0]
-	minval_index = np.max([val-CONST, 0])
-	maxval_index = np.min([val+1+CONST, len(midvalues)-1])
-
-	return (midvalues[minval_index], midvalues[maxval_index])
-
-
-
-#FILE = sys.argv[1]
 flist = glob.glob("./logs/*.msl")
 
 
@@ -132,7 +94,6 @@ for fname in flist:
 
 data_raw = pd.concat(pd_frames)
 
-#print(data_raw.describe)
 
 data_raw.AFR = data_raw.AFR.shift(SHIFT_AFR)
 data_raw.Lambda = data_raw.Lambda.shift(SHIFT_AFR)
@@ -140,12 +101,8 @@ data_raw.Lambda = data_raw.Lambda.shift(SHIFT_AFR)
 
 
 
-data = data_raw[(data_raw.Gwarm==100) & (data_raw.RPM>0)& (data_raw.DFCO==0)& (data_raw['rpm/s']>=-1000) & (data_raw['Accel Enrich']==100)]
+data = data_raw[(data_raw.Gwarm==100) & (data_raw.RPM>0)& (data_raw.DFCO==0)& (data_raw['rpm/s']>=-1000) & (data_raw['Accel Enrich']==100) & (data_raw.TPS>0.0)]
 data = data.dropna()
-#print(data.describe)
-
-#data = data.assign(corr_coef = lambda x: x['AFR']/x['AFR Target'])
-#data['corr_coef'] = data.apply(lambda row: row['AFR']/row['AFR Target'], axis=1)
 data['afr_target_func'] = data.apply(lambda row: AFR_TABLE_FUNC(row['RPM'], row['MAP'])[0], axis=1)
 data['corr_coef'] = data.apply(lambda row: row['AFR']/row['afr_target_func'], axis=1)
 data['ve_predicted'] = data.apply(lambda row: row['VE1']*row['corr_coef'], axis=1)
@@ -165,65 +122,39 @@ data_points_amount = np.empty((KPA_BINS.size,RPM_BINS.size), dtype = object)
 
 for i in range(KPA_BINS.size):
 	for j in range(RPM_BINS.size):
-		kpa_min, kpa_max = getValueFromBin(i, KPA_BINS)
-		rpm_min, rpm_max = getValueFromBin(j, RPM_BINS)
+		kpa_min, kpa_max = np.max([KPA_BINS[i]-KPA_MARGIN, 10]),  np.min([KPA_BINS[i]+KPA_MARGIN, 900])
+		rpm_min, rpm_max = np.max([RPM_BINS[j]-RPM_MARGIN,100]),  np.min([RPM_BINS[j]+RPM_MARGIN, 10000])
 		pandas_frames[i][j] = data[(data.MAP>=kpa_min) & (data.MAP<=kpa_max) & (data.RPM>=rpm_min) & (data.RPM<=rpm_max)]
 		data_points_amount[i][j] = len(pandas_frames[i][j])
 		if data_points_amount[i,j]>=HITS_NEEDED:
 			AFR_achieved[i][j] = pandas_frames[i][j].AFR.median()
-			VE_achieved[i][j]=pandas_frames[i][j].VE1.median()
 			VE_predicted[i][j]=pandas_frames[i][j]['ve_predicted'].quantile(QUANTILE)
-			VE_predicted_std[i][j]=pandas_frames[i][j]['ve_predicted'].std()
 			Lambda_achieved[i][j]=pandas_frames[i][j]['Lambda'].median()
-			Lambda_achieved_std[i][j]=pandas_frames[i][j]['Lambda'].std()
 
 
 np.set_printoptions(floatmode = 'fixed',precision = 2, linewidth = 150, suppress = True)
 
-print("VE from tune:")
+print("VE opened from tune:")
 print(np.flipud(VE_TABLE.astype(np.float64)))
 
 
-print("Data points amount:")
+print("Data points amount during RUN:")
 print(np.flipud(data_points_amount.astype(int)))
-
-
-print("VE during RUN:")
-print(np.flipud(VE_achieved.astype(np.float64)))
-
 
 
 corrected_ve = VE_predicted.astype(np.float64)
 corrected_ve = np.nan_to_num(corrected_ve.astype(np.float64))
 corrected_ve[corrected_ve==0] = VE_TABLE[corrected_ve==0]
 
-print("VE CORRECTED by coef:")
+print("VE predicted:")
 print(np.flipud((corrected_ve).astype(np.float64)))
 
 
-blurred_ve = np.round(gaussian_filter(corrected_ve, sigma=BLURR_FACTOR)).astype(int)
-blurred_ve[corrected_ve==0] = VE_TABLE[corrected_ve==0]
-print("VE blurred:")
-print(np.flipud((blurred_ve).astype(np.float64)))
-
-
-
-
-print("predicted VE STD dev:")
-print(np.flipud((VE_predicted_std).astype(np.float64)))
-
-
-print("AFR during RUN:")
+print("AFR achieved during RUN:")
 print(np.flipud(AFR_achieved.astype(np.float64)))
 
-
-
-
-print("Lambda achieved:")
+print("Lambda achieved during RUN:")
 print(np.flipud(Lambda_achieved.astype(np.float64)))
-
-print("Lambda STD dev:")
-print(np.flipud(Lambda_achieved_std.astype(np.float64)))
 
 print("VE increased +:")
 print(np.flipud(np.round(corrected_ve-VE_TABLE)))
@@ -236,6 +167,3 @@ print(np.flipud(corrected_ve/VE_TABLE))
 
 export(RPM_BINS, KPA_BINS, AFR_TABLE, './exports/AFR_EXPORT.table', dtype=float)
 export(RPM_BINS, KPA_BINS, corrected_ve, './exports/VE_EXPORT.table')
-export(RPM_BINS, KPA_BINS, blurred_ve, './exports/VE_EXPORT_blurred.table')
-
-
