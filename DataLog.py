5d145c2feb3431c072c4f22537bc466f67d8ab7e
XPbIM3 <xpbim3@gmail.com>
2023-10-06 14:42:45 +0500

    switch to double

--- DataLog.py
+++ DataLog.py
@@ -36,11 +36,11 @@ RPM_BINS_IGN, KPA_BINS_IGN, _, IGN_TABLE_FUNC = getTable(F_NAME,IGN_TABLE_DICT)
 
 
 if GENERATE_AXES == False:
-	KPA_BINS = KPA_BINS_VE
-	RPM_BINS = RPM_BINS_VE
+    KPA_BINS = KPA_BINS_VE
+    RPM_BINS = RPM_BINS_VE
 else:
-	KPA_BINS = ((np.round(np.linspace(CURRENT_CONFIG['KPA_MIN'],CURRENT_CONFIG['KPA_MAX'], 16))//2)*2).astype(int)
-	RPM_BINS = ((np.round(np.linspace(CURRENT_CONFIG['RPM_MIN'],CURRENT_CONFIG['RPM_MAX'], 16))//100)*100).astype(int)
+    KPA_BINS = ((np.round(np.linspace(CURRENT_CONFIG['KPA_MIN'],CURRENT_CONFIG['KPA_MAX'], 16))//2)*2).astype(int)
+    RPM_BINS = ((np.round(np.linspace(CURRENT_CONFIG['RPM_MIN'],CURRENT_CONFIG['RPM_MAX'], 16))//100)*100).astype(int)
 
 
 KPA_MARGIN = ((CURRENT_CONFIG['KPA_MAX'] - CURRENT_CONFIG['KPA_MIN'])//15)*MARGIN
@@ -57,31 +57,31 @@ VE_TABLE = VE_TABLE_FUNC(RPM_BINS, KPA_BINS)
 IGN_TABLE = IGN_TABLE_FUNC(RPM_BINS, KPA_BINS)
 
 if GENERATE_AFR_TABLE:
-	AFR_TABLE = np.array([np.linspace(CURRENT_CONFIG['AFR_MIN'], CURRENT_CONFIG['AFR_MAX'], 16)]*16).transpose()
-	AFR_TABLE_FUNC = scipy.interpolate.interp2d(RPM_BINS, KPA_BINS, AFR_TABLE)
+    AFR_TABLE = np.array([np.linspace(CURRENT_CONFIG['AFR_MIN'], CURRENT_CONFIG['AFR_MAX'], 16)]*16).transpose()
+    AFR_TABLE_FUNC = scipy.interpolate.interp2d(RPM_BINS, KPA_BINS, AFR_TABLE)
 else:
-	AFR_TABLE = AFR_TABLE_FUNC(RPM_BINS, KPA_BINS)
+    AFR_TABLE = AFR_TABLE_FUNC(RPM_BINS, KPA_BINS)
 
 
 def export(RPMS, KPAS, ZS, fname, dtype=int):
-	with open('./templates/VE.table.template', 'r') as t:
-		templ = t.read()
+    with open('./templates/VE.table.template', 'r') as t:
+        templ = t.read()
 
-	xax = "\n"+("\n".join(list(map(str, RPMS.astype(int)))))+"\n"
-	yax = "\n"+("\n".join(list(map(str, KPAS.astype(int)))))+"\n"
-	zax = "\n"+str(ZS.astype(dtype)).strip('[ ]').replace('[','').replace(']', '')+"\n"
+    xax = "\n"+("\n".join(list(map(str, RPMS.astype(int)))))+"\n"
+    yax = "\n"+("\n".join(list(map(str, KPAS.astype(int)))))+"\n"
+    zax = "\n"+str(ZS.astype(dtype)).strip('[ ]').replace('[','').replace(']', '')+"\n"
 
 
-	templ = templ.replace('_XAXIS_', xax)
-	templ = templ.replace('_YAXIS_', yax)
-	templ = templ.replace('_ZAXIS_', zax)
+    templ = templ.replace('_XAXIS_', xax)
+    templ = templ.replace('_YAXIS_', yax)
+    templ = templ.replace('_ZAXIS_', zax)
 
-	with open(fname, 'w') as t:
-		t.write(templ)
+    with open(fname, 'w') as t:
+        t.write(templ)
 
 
 def getBinFromValue(value:float, bins:np.ndarray):
-	return np.argmin(np.abs(value-bins))
+    return np.argmin(np.abs(value-bins))
 
 
 flist = glob.glob("./input/*.msl")
@@ -89,8 +89,8 @@ flist = glob.glob("./input/*.msl")
 
 pd_frames = []
 for fname in flist:
-	fr = pd.read_csv(fname, sep='\t', header=0, skiprows=[0,1,3])
-	pd_frames.append(fr)
+    fr = pd.read_csv(fname, sep='\t', header=0, skiprows=[0,1,3])
+    pd_frames.append(fr)
 
 data_raw = pd.concat(pd_frames)
 
@@ -112,52 +112,54 @@ data['corr_coef'] = data.apply(lambda row: row['AFR']/row['afr_target_func'], ax
 data['ve_predicted'] = data.apply(lambda row: row['VE1']*row['corr_coef'], axis=1)
 
 
-AFR_achieved =  np.full((KPA_BINS.size,RPM_BINS.size),np.nan, dtype = float)
-Lambda_achieved =  np.full((KPA_BINS.size,RPM_BINS.size),np.nan, dtype = float)
-VE_predicted_weightened = np.zeros((KPA_BINS.size,RPM_BINS.size), dtype = int)
+AFR_achieved =  np.full((KPA_BINS.size, RPM_BINS.size), np.nan, dtype=np.double)
+RPMdot_achieved =  np.full((KPA_BINS.size, RPM_BINS.size), np.nan, dtype=np.double)
+Lambda_achieved =  np.full((KPA_BINS.size, RPM_BINS.size), np.nan, dtype=np.double)
+VE_predicted_weightened = np.zeros((KPA_BINS.size, RPM_BINS.size), dtype=int)
 
 
 for i in range(KPA_BINS.size):
-	for j in range(RPM_BINS.size):
-		kpa_min, kpa_max = np.max([KPA_BINS[i]-KPA_MARGIN, 0]),  np.min([KPA_BINS[i]+KPA_MARGIN, 900])
-		rpm_min, rpm_max = np.max([RPM_BINS[j]-RPM_MARGIN, 0]),  np.min([RPM_BINS[j]+RPM_MARGIN, 10000])
-		pd_local_frame = data[(data.MAP>=kpa_min) & (data.MAP<=kpa_max) & (data.RPM>=rpm_min) & (data.RPM<=rpm_max)]
-		data_points_amount = len(pd_local_frame)
-		if data_points_amount>=HITS_NEEDED:
-			rpms_np = pd_local_frame['RPM'].to_numpy(dtype=int)
-			kpas_np = pd_local_frame['MAP'].to_numpy(dtype=int)
-			ves_np = pd_local_frame['ve_predicted'].to_numpy(dtype=float)
-			x_dist = np.abs(rpms_np - RPM_BINS[j])/RPM_MARGIN
-			y_dist = np.abs(kpas_np - KPA_BINS[i])/KPA_MARGIN
-			weights = 1-((np.sqrt(x_dist**2 + y_dist**2))/(np.sqrt(2.0)))
-			weights = weights/np.sum(weights)
-			VE_predicted_weightened[i][j] = int(np.round(np.sum(ves_np * weights)))
-			AFR_achieved[i][j] = float(pd_local_frame.AFR.median())
-			Lambda_achieved[i][j] = float(pd_local_frame['Lambda'].median())
+    for j in range(RPM_BINS.size):
+        kpa_min, kpa_max = np.max([KPA_BINS[i]-KPA_MARGIN, 0]),  np.min([KPA_BINS[i]+KPA_MARGIN, 900])
+        rpm_min, rpm_max = np.max([RPM_BINS[j]-RPM_MARGIN, 0]),  np.min([RPM_BINS[j]+RPM_MARGIN, 10000])
+        pd_local_frame = data[(data.MAP>=kpa_min) & (data.MAP<=kpa_max) & (data.RPM>=rpm_min) & (data.RPM<=rpm_max)]
+        data_points_amount = len(pd_local_frame)
+        if data_points_amount>=HITS_NEEDED:
+            rpms_np = pd_local_frame['RPM'].to_numpy(dtype=np.double)
+            kpas_np = pd_local_frame['MAP'].to_numpy(dtype=np.double)
+            ves_np = pd_local_frame['ve_predicted'].to_numpy(dtype=np.double)
+            x_dist = np.abs(rpms_np - np.double(RPM_BINS[j])) / np.double(RPM_MARGIN)
+            y_dist = np.abs(kpas_np - np.double(KPA_BINS[i])) / np.double(KPA_MARGIN)
+            weights = 1.0 - ((np.sqrt(x_dist**2 + y_dist**2)) / (np.sqrt(2.0)))
+            weights_norm = weights / np.sum(weights)
+            VE_predicted_weightened[i][j] = int(np.round(np.sum(ves_np * weights_norm)))
+            AFR_achieved[i][j] = float(pd_local_frame.AFR.median())
+            Lambda_achieved[i][j] = float(pd_local_frame['Lambda'].median())
+            RPMdot_achieved[i][j] = pd_local_frame['rpm/s'].median()
 
 
 weighted_ve = VE_predicted_weightened.copy()
 weighted_ve[weighted_ve==0] = VE_TABLE[weighted_ve==0]
 
 
-def plot_array(x_axis: list, y_axis: list, arr: np.ndarray):
-	arr = np.flipud(arr)
-	x_axis_size = arr.shape[0]
-	y_axis_size = arr.shape[0]
-	x_ticks = np.arange(0, x_axis_size)
-	y_ticks = np.arange(0, y_axis_size)
-	x_labels = [str(i) for i in x_axis]
-	y_labels = [str(i) for i in y_axis][::-1]
+def saveFig(x_axis: list, y_axis: list, arr: np.ndarray, fname: str):
+    arr = np.flipud(arr)
+    x_axis_size = arr.shape[0]
+    y_axis_size = arr.shape[0]
+    x_ticks = np.arange(0, x_axis_size)
+    y_ticks = np.arange(0, y_axis_size)
+    x_labels = [str(i) for i in x_axis]
+    y_labels = [str(i) for i in y_axis][::-1]
 
-	plt.figure(figsize=(10, 6))
-	plt.imshow(arr, interpolation='none')
-	for i in range(x_axis_size):
-		for j in range(y_axis_size):
-			plt.text(j, i, f'{(arr[i, j]):.2f}', ha='center', va='center', color='black', size=5)
+    plt.figure(figsize=(10, 6))
+    plt.imshow(arr, interpolation='none')
+    for i in range(x_axis_size):
+        for j in range(y_axis_size):
+            plt.text(j, i, f'{(arr[i, j]):.2f}', ha='center', va='center', color='black', size=5)
 
-	plt.xticks(ticks=x_ticks, labels=x_labels, size=6)
-	plt.yticks(ticks=y_ticks, labels=y_labels, size=6)
-	plt.show()
+    plt.xticks(ticks=x_ticks, labels=x_labels, size=6)
+    plt.yticks(ticks=y_ticks, labels=y_labels, size=6)
+    plt.savefig(fname)
 
 
 
@@ -172,8 +174,10 @@ print(np.flipud(Lambda_achieved))
 
 print("VE increased +:")
 
-#ve_delta = np.flipud(np.round(weighted_ve-VE_TABLE))
-#plot_array(RPM_BINS, KPA_BINS, ve_delta)
+ve_delta = np.flipud(np.round(weighted_ve-VE_TABLE))
+saveFig(RPM_BINS, KPA_BINS, ve_delta, 'delta.png')
+
+saveFig(RPM_BINS, KPA_BINS, RPMdot_achieved, 'rpms.png')
 
 print(np.flipud(np.round(weighted_ve-VE_TABLE)))
 
